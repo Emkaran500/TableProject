a025220d9d4fba942c877ad40323016e5a0b3fb2
Emil B <emkaran_500@mail.ru>
2023-11-24 19:30:04 +0400

    Add Listener

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System.Net;
+
+HttpListener httpListener = new HttpListener();
+
+const int port = 9999;
+
+httpListener.Prefixes.Add($"http://*:{port}/");
+httpListener.Start();
+System.Console.WriteLine($"Server started on '{port}' port");
+
+while (true)
+{
+    var context = await httpListener.GetContextAsync();
+    var rawUrl = context.Request.RawUrl?.Trim('/').ToLower();
+    using var writer = new StreamWriter(context.Response.OutputStream);
+    var rawItems = rawUrl?.Split('/');
+}
