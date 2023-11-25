4884043c84ed3abed05b0128d325b5b9d3df5cfd
Nijat Muradov <live4ever4@gmail.com>
2023-11-26 00:42:46 +0400

    Edit code

---
+++ ClientApp/Models/HttpClientInstance.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ClientApp.Models
+{
+    public class HttpClientInstance
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+        public static HttpClient Instance = httpClient;
+    }
+}
--- ClientApp/Views/AuthorizationView.xaml.cs
+++ ClientApp/Views/AuthorizationView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 
 namespace ClientApp.Views
@@ -22,6 +23,29 @@ namespace ClientApp.Views
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var login = loginTextBox.Text;
+                var password = passswordTextBox.Text;
+
+                if(string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+                {
+                    throw new ArgumentNullException($"{nameof(login)} or/and {nameof(password)} is empty");
+                }
+
+                this.Hide();
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
